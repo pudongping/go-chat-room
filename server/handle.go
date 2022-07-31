cf99d40f33a779ebadd5a4dcfba9c3ef5d7c0867
pudongping <276558492@qq.com>
2022-07-31 23:28:44 +0800

    Update handle.go

--- server/handle.go
+++ server/handle.go
@@ -1,43 +1,16 @@
 package server
 
 import (
-	"os"
-	"path/filepath"
+	"net/http"
+
+	"github.com/pudongping/go-chat-room/logic"
 )
 
-var rootDir string
-
 func RegisterHandle() {
-	inferRootDir()
-
 	// 广播消息处理
-	// go logic.Broadcaster.Start()
+	go logic.Broadcaster.Start()
 
-	// 	http.HandleFunc("/", homeHandleFunc)
-	//	http.HandleFunc("/ws", WebSocketHandleFunc)
-
-}
-
-// 推断出项目根目录
-func inferRootDir() {
-	// 通过 os.Getwd() 获取当前工作目录
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	var infer func(d string) string
-	infer = func(d string) string {
-		// 这里要确保项目根目录下存在 template 目录
-		if exists(d + "/template") {
-			return d
-		}
-		return infer(filepath.Dir(d))
-	}
-
-	rootDir = infer(cwd)
-}
-
-func exists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil || os.IsExist(err)
+	http.HandleFunc("/", homeHandleFunc)
+	http.HandleFunc("/user_list", userListHandleFunc)
+	http.HandleFunc("/ws", WebSocketHandleFunc)
 }
